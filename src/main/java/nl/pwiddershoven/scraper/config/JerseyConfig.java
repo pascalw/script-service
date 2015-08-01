6568176297194c2129dd5b00f14dbe38f48a971f
Pascal Widdershoven <pascal@kabisa.nl>
2015-08-01 21:51:20 +0200

    Let Spring boot serve the configurator

--- src/main/java/nl/pwiddershoven/scraper/config/JerseyConfig.java
+++ src/main/java/nl/pwiddershoven/scraper/config/JerseyConfig.java
@@ -5,6 +5,7 @@ import nl.pwiddershoven.scraper.controller.ScrapeController;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,9 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(JacksonFeature.class);
+
         property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+        property(ServletProperties.FILTER_FORWARD_ON_404, true);
 
         register(ScrapeController.class);
     }
