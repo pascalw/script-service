2936c3e70ddf89f229bfda4ef2df7372d809981b
Pascal Widdershoven <pascal@kabisa.nl>
2015-08-13 02:49:22 +0200

    Update hetzner script with parsed date

--- scripts/hetzner.js
+++ scripts/hetzner.js
@@ -8,22 +8,27 @@ var feed = newFeed()
 
 var nextOfType = function(element, type) {
   var node = element.nextElementSibling();
-  while (node != null && !node.tagName().equalsIgnoreCase(type)) {
+  while (node !== null && !node.tagName().equalsIgnoreCase(type)) {
       node = node.nextElementSibling();
   }
   return node;
 };
 
+var df = new java.text.SimpleDateFormat('dd MMMM yyyy', new java.util.Locale('en', ''));
+
 page.select("#content-noright h4:gt(2)").stream().forEach(function(h4) {
-  var title = h4.text();
+  var title = h4.ownText();
   var nextP = nextOfType(h4, 'p');
   var description = nextP.text();
+  var publishedDateString = h4.select('em').text().replace('from: ', '');
+  var publishedDate = df.parse(publishedDateString);
+  
   var link = nextP.siblingElements().select('a').first().attr('href').toString();
 
   var entry = feed.newEntry()
     .setTitle(title)
     .setLink(link)
-    .setPublishedDate(new java.util.Date())
+    .setPublishedDate(publishedDate)
     .setDescription(description);
 
   feed.addEntry(entry);
