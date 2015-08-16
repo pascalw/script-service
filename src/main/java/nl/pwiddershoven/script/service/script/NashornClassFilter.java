d2744e4ec8cf1206a6ef84b0f002a8b9d4c8b3e3
Pascal Widdershoven <pascal@kabisa.nl>
2015-08-17 01:12:39 +0200

    Remove unneeded classes from NashornClassFilter
    Only classes that are directly referenced from scripts should be
    whitelisted. DSL methods provided from Java can access any classes.

--- src/main/java/nl/pwiddershoven/script/service/script/NashornClassFilter.java
+++ src/main/java/nl/pwiddershoven/script/service/script/NashornClassFilter.java
@@ -9,15 +9,10 @@ import com.google.common.collect.Sets;
 public class NashornClassFilter implements ClassFilter {
     private static final Set<String> CLASS_WHITELIST = Sets.newHashSet(
             "java.util.Date",
-            "java.lang.Object",
             "java.lang.String",
-            "java.util.ArrayList",
-            "java.util.HashMap",
             "java.util.Locale",
             "java.util.stream.Collectors",
-            "java.text.SimpleDateFormat",
-            "org.jsoup.nodes.Document",
-            "org.jsoup.select.Elements");
+            "java.text.SimpleDateFormat");
 
     @Override
     public boolean exposeToScripts(String clazz) {
