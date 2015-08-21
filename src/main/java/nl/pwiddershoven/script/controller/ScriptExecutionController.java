485bb51f6675460e25e0812552e16bbf444fac19
Pascal Widdershoven <pascal@kabisa.nl>
2015-08-21 20:24:41 +0200

    Remove consumes JSON restriction from ScriptExecutionController
    Any media type is fine, if a script wishes to handle it

--- src/main/java/nl/pwiddershoven/script/controller/ScriptExecutionController.java
+++ src/main/java/nl/pwiddershoven/script/controller/ScriptExecutionController.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@Consumes(MediaType.APPLICATION_JSON)
 @Path("/")
 public class ScriptExecutionController {
     private final Logger logger = Logger.getLogger(ScriptExecutionController.class);
